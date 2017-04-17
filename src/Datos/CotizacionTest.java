ba6f9b45c370bdf36920a4a240dbad7561e8625b
cristhianzb <cristhianzentenob@gmail.com>
2017-04-16 21:49:23 -0400

    CotizacionTest corregido
    Pasa los test de CotizacionTest

--- src/Datos/CotizacionTest.java
+++ src/Datos/CotizacionTest.java
@@ -15,7 +15,7 @@ public class CotizacionTest {
 		Producto p = new Producto(10, "Laptop", "HP", "SGHA", 1500, 1000, new Almacen("Sacaba", "Sacaba KM 10", "44444"));
 		
 		Object[] datos={"00002",new Date("2017/04/15"),new Date("2017/05/15"),(long)175072028,"Toyota", p ,(double)15000,1, (double)15000,(double)104400};
-		Cotizacion cotizacion = new Cotizacion((String)datos[0], (Date)datos[1], (Date)datos[2], (long)datos[3], (String)datos[4], (Producto)datos[5], (double)datos[6], (int)datos[7], 6); 
+		Cotizacion cotizacion = new Cotizacion((String)datos[0], (Date)datos[1], (Date)datos[2], (long)datos[3], (String)datos[4], (Producto)datos[5], (double)datos[6], (int)datos[7], 6.96); 
 		assertEquals(15000, cotizacion.importeTotal(),0.001);
 		assertEquals(104400, cotizacion.importeTotalBs(),0.001);	
 	}
